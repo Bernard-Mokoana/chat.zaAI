8ec36bb2317b277eabe750611d4d747e4d20bc47
Bernard <bernarddira@gmail.com>
2026-05-09 23:59:31 +0200

    feat: add token payload and update get token

--- server/src/socket/utils.py
+++ server/src/socket/utils.py
@@ -1,16 +1,22 @@
 from fastapi import WebSocket, status, Query
 from typing import Optional
-from ..redis.config import Redis
+from src.utils.token import Token
+
+token_util = Token()
 
 async def get_token(websocket: WebSocket, token: Optional[str] = Query(None)):
     if token is None or token == "":
         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
         return
 
-    redis_client = await Redis().create_connection()
-    isexists = await redis_client.exists(token)
+    try:
+        payload = token_util.decode_access_token(token)
+    except Exception:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
 
-    if isexists == 1:
-        return token
+    if not payload.get("id"):
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
 
-    await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+    return payload
