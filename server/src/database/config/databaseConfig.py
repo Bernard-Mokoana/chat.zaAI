72331929d76665e71e58e3afe002800dbe08ddfb
Bernard <bernarddira@gmail.com>
2026-05-25 21:47:20 +0200

    feat: config primary db and read replica(postgres)

--- server/src/database/config/databaseConfig.py
+++ server/src/database/config/databaseConfig.py
@@ -6,18 +6,29 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+PRIMARY_URL = os.environ.get("DATABASE_PRIMARY_URL")
+REPLICA_URL = os.environ.get("DATABASE_REPLICA_URL")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set.")
+if not PRIMARY_URL or not REPLICA_URL:
+    raise ValueError("Both DATABASE_PRIMARY_URL and DATABASE_REPLICA_URL environment variables must be set.")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine_primary = create_engine(PRIMARY_URL, pool_pre_ping=True)
+engine_replica = create_engine(REPLICA_URL, pool_pre_ping=True)
+
+SessionPrimary = sessionmaker(autocommit=False, autoflush=False, bind=engine_primary) 
+SessionReplica = sessionmaker(autocommit=False, autoflush=False, bind=engine_replica)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
+def get_write_db():
+    db = SessionPrimary()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_read_db():
+    db = SessionReplica()
     try:
         yield db
     finally:
